a414a6a9e930666391ba14750a335e1fb94d45cb
Parag-Katiyar <paragkatiyar473@gmail.com>
2026-08-08 02:10:46 +0530

    Add: decode-ways - submission-9

---
+++ Data Structures & Algorithms/decode-ways/submission-9.py
@@ -0,0 +1,26 @@
+#Clener Version# 
+
+class Solution:
+    def numDecodings(self, s: str) -> int:
+        n = len(s)
+        memo = {}
+
+        def num(i):
+            if i == n:
+                return 1
+
+            if s[i] == '0':
+                return 0
+
+            if i in memo:
+                return memo[i]
+
+            ways = num(i + 1)
+
+            if i + 1 < n and 10 <= int(s[i:i+2]) <= 26:
+                ways += num(i + 2)
+
+            memo[i] = ways
+            return ways
+
+        return num(0)
