a5591619cd365baa52e4b2fc888b1a5d2263f480
Parag-Katiyar <paragkatiyar473@gmail.com>
2026-08-08 01:54:38 +0530

    Add: decode-ways - submission-6

---
+++ Data Structures & Algorithms/decode-ways/submission-6.py
@@ -0,0 +1,53 @@
+class Solution:
+    def numDecodings(self, s: str) -> int:
+
+        l = len(s)
+
+        if l == 1: 
+            if int(s[0]) == 0: 
+                return 0
+            return 1
+        
+
+        memo = {}
+
+        def num(i): 
+
+
+            if i in memo: 
+                return memo[i]
+
+
+            if i >= l: 
+                return 1 #Needed correction 
+
+            if int(s[i]) == 0: 
+                return 0 
+
+            if int(s[i]) == 1 and i <=l-2:
+                n_i = num(i+1) + num(i+2)
+            if int(s[i]) == 1 and i == l-1:
+                return 1
+
+            if i <= l-2 and int(s[i]) == 2 and int(s[i+1]) < 7:
+                n_i = num(i+1) + num(i+2)
+
+            if i <= l-2 and int(s[i]) == 2 and int(s[i+1]) >= 7: 
+                n_i = num(i+1) 
+
+            if i == l-1 and int(s[i]) == 2:
+                n_i = num(i+1)
+            
+            if int(s[i]) > 2: 
+                n_i = num(i+1) #needed corection
+
+            memo[i] = n_i
+            return n_i
+
+        return num(0)
+
+
+
+
+        
+#No need to map just conditions
