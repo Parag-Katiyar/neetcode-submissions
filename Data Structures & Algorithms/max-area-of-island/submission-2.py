7f50bbfc26022a4726b80fb0508578297e048e91
Parag-Katiyar <paragkatiyar473@gmail.com>
2026-09-06 00:06:14 +0530

    Add: max-area-of-island - submission-2

---
+++ Data Structures & Algorithms/max-area-of-island/submission-2.py
@@ -0,0 +1,49 @@
+class Solution:
+    def maxAreaOfIsland(self, grid: List[List[int]]) -> int:
+
+        area_max = 0
+        area = 0  
+       
+
+        l1 = len(grid)
+        l2 = len(grid[0])
+
+        def areax(i,j):
+            
+            nonlocal area 
+            
+            
+            if i <= l1-1 and 0 <= i: 
+                if j <= l2-1 and 0 <= j:
+
+                    if grid[i][j] == 2 or grid[i][j] == 0: 
+                        return
+
+                    elif grid[i][j] == 1: 
+                        area = area + 1 
+                        grid[i][j] = 2
+                    
+
+                    areax(i+1, j)
+                    areax(i, j+1)
+                    areax(i-1, j)
+                    areax(i, j-1)
+            return 
+
+        for j in range(0,l2): 
+            for i in range(0,l1): 
+
+                areax(i,j)
+
+                if area > area_max: 
+                    area_max = area
+
+                area = 0 
+            
+        return area_max
+
+                
+
+
+
+        
