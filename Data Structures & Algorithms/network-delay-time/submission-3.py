4c35b7777ebd2c9e96d8c20ea7b8284ff9acc44e
Parag-Katiyar <paragkatiyar473@gmail.com>
2026-09-12 01:21:15 +0530

    Add: network-delay-time - submission-3

---
+++ Data Structures & Algorithms/network-delay-time/submission-3.py
@@ -0,0 +1,54 @@
+import heapq
+class Solution:
+    def networkDelayTime(self, times: List[List[int]], n: int, k: int) -> int:
+    
+        mapx = {}
+        for i in times: 
+            if i[0] not in mapx: 
+                mapx[i[0]] = []
+            mapx[i[0]].append([i[1],i[2]])
+
+        heap_one = []
+        time = {}
+
+        time[k] = 0
+        heapq.heappush(heap_one, (0,k))
+
+        while heap_one: 
+
+            time_curr,x = heapq.heappop(heap_one)
+            
+            if time_curr > time[x]:
+                continue
+
+            # Relaxing and pushing
+            for i in mapx.get(x,[]):
+
+                if i[0] not in time: 
+                    time[i[0]] =  i[1] + time_curr
+                    heapq.heappush(heap_one,(time[i[0]], i[0]))
+
+
+                else: 
+                    if i[1] + time_curr < time[i[0]]: 
+
+                        time[i[0]] =  i[1] + time_curr
+
+                        heapq.heappush(heap_one,(time[i[0]], i[0]))
+
+        if len(time) < n: 
+            return -1
+        else: 
+            return max(time.values())
+
+
+
+
+
+
+
+
+
+
+
+            
