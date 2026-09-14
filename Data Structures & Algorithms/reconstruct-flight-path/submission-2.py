f73245fc5fbbfa82ea33e5e1ac2bbff3862d6fcc
Parag-Katiyar <paragkatiyar473@gmail.com>
2026-09-15 00:44:24 +0530

    Add: reconstruct-flight-path - submission-2

---
+++ Data Structures & Algorithms/reconstruct-flight-path/submission-2.py
@@ -0,0 +1,33 @@
+class Solution:
+    def findItinerary(self, tickets: List[List[str]]) -> List[str]:
+        mapx = {}
+
+        for i in tickets:
+            if i[0] not in mapx: 
+                mapx[i[0]] = []
+                mapx[i[0]].append(i[1])
+            else: 
+                mapx[i[0]].append(i[1])
+        
+        for x in mapx: ### SYNTAX ####
+            mapx[x].sort()
+            mapx[x].reverse()
+            
+        stack = ["JFK"]
+        answer = []
+
+
+        while stack:
+
+            if mapx.get(stack[-1],[]): ### SYNTAX and .get !!!!!
+
+                x = mapx[stack[-1]].pop()
+                stack.append(x)
+
+            else: 
+                ans = stack.pop()
+                answer.append(ans)
+
+        answer.reverse() # Syantax !!!!!! 
+        return answer
+   
